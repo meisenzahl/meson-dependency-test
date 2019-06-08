bebbdcb0c6430a416768ac617d1621530436ccaa
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2019-06-08 09:11:52 +0200

    run test cases with meson with support for nested subprojects

--- test.py
+++ test.py
@@ -4,25 +4,49 @@ import shutil
 from subprocess import Popen, PIPE, STDOUT
 import unittest
 
-def runCmd(cmd):
+def runCmd(cmd, exitOnError=False, verbose=False):
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
-    output = p.communicate()
-    
-    if p.returncode != 0:
-        print(output)
-        exit(p.returncode)
+    output = p.communicate()[0]
+
+    if p.returncode == 0 and verbose:
+        print(output.decode("utf-8"))
+    elif p.returncode != 0:
+        print(output.decode("utf-8"))
+
+        if exitOnError:
+            exit(p.returncode)
+
+    return p.returncode
 
 def updateMeson():
-    runCmd("cd meson && git checkout nested-subprojects && git pull")
+    runCmd(
+        "cd meson && git checkout nested-subprojects && git pull",
+        exitOnError=True
+    )
 
 def checkoutMeson():
-    runCmd("git clone -b nested-subprojects git@github.com:semasquare/meson.git")
+    runCmd(
+        "git clone -b nested-subprojects git@github.com:semasquare/meson.git",
+        exitOnError=True
+    )
 
-def cleanSubprojectsFolder(dir):
-    if os.path.exists(dir):
-        for f in os.listdir(dir):
+def runMeson(dir):
+    meson = "python3 {}".format(os.path.abspath("meson/__main__.py"))
+
+    return runCmd("cd {} && {} build".format(dir, meson))
+
+def cleanProjectDirectory(dir):
+    build = os.path.join(dir, "build")
+
+    if os.path.exists(build):
+        shutil.rmtree(build)
+
+    subprojects = os.path.join(dir, "subprojects")
+
+    if os.path.exists(subprojects):
+        for f in os.listdir(subprojects):
             if not f.endswith(".wrap"):
-                shutil.rmtree(os.path.join(dir, f))
+                shutil.rmtree(os.path.join(subprojects, f))
 
 class NestedSubprojects(unittest.TestCase):
     @classmethod
@@ -34,20 +58,20 @@ class NestedSubprojects(unittest.TestCase):
             print("Checking out meson with support for nested subprojects")
             checkoutMeson()
 
-    def testCase01(self):
-        cleanSubprojectsFolder("test/nested-subprojects/01/subprojects")
+    def testCase01(self, dir="test/nested-subprojects/01"):
+        cleanProjectDirectory(dir)
 
-        self.assertTrue(False)
+        self.assertEqual(runMeson(dir), 0)
 
-    def testCase02(self):
-        cleanSubprojectsFolder("test/nested-subprojects/02/subprojects")
+    def testCase02(self, dir="test/nested-subprojects/02"):
+        cleanProjectDirectory(dir)
 
-        self.assertTrue(False)
+        self.assertEqual(runMeson(dir), 0)
 
-    def testCase03(self):
-        cleanSubprojectsFolder("test/nested-subprojects/03/subprojects")
+    def testCase03(self, dir="test/nested-subprojects/03"):
+        cleanProjectDirectory(dir)
 
-        self.assertTrue(False)
+        self.assertEqual(runMeson(dir), 0)
 
 if __name__ == "__main__":
     unittest.main()
